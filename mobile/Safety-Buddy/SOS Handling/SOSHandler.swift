d873baaea875570a08fa3871774392c3971c09b9
Om Chachad <96822675+OmChachad@users.noreply.github.com>
2025-10-25 05:04:16 -0700

    Implement 911 call functionality in SOSHandler
    Added logic to call 911 using UIApplication and handle success or failure when opening the tel://911 URL. This enhances the SOSHandler by enabling direct emergency calls from the app.

--- mobile/Safety-Buddy/SOS Handling/SOSHandler.swift
+++ mobile/Safety-Buddy/SOS Handling/SOSHandler.swift
@@ -6,10 +6,23 @@
 //
 
 import Foundation
+import UIKit
 
 struct SOSHandler {
     func call911() {
-        
+        if let url = URL(string: "tel://911") {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:]) { success in
+                    if success {
+                        print("URL opened successfully")
+                    } else {
+                        print("Failed to open URL")
+                    }
+                }
+            } else {
+                print("Cannot open this URL")
+            }
+        }
     }
     
     func whistle() {
